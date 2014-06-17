aa557276cdb17a178fbd7c4bbe567effce5af251
root <root@PMserv1.fda.gov>
2014-06-17 18:44:02 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Live Helper Chat
+
+This repo contains Montgomery Blair High School's implementation of the open source
+Live Helper Chat (livehelperchat.com).  This implementation adds the following features:
+
+-Login form to restrict users to students
+-Menu allowing users to select a subject from a list
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Online Tutoring & Study Help - Montgomery Blair High School</title>
+        <meta name="description" content="Custom Login Form Styling with CSS3" />
+        <meta name="keywords" content="css3, login, form, custom, input, submit, button, html5, placeholder" />
+        <meta name="author" content="Codrops" />
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+		<script src="js/modernizr.custom.63321.js"></script>
+		<!--[if lte IE 7]><style>.main{display:none;} .support-note .note-ie{display:block;}</style><![endif]-->
+    </head>
+    <body>
+        <div class="container">
+		
+			<!-- Codrops top bar -->
+            <div class="codrops-top">
+                <!--
+				<a href="http://tympanus.net/Tutorials/RealtimeGeolocationNode/">
+                    <strong>&laquo; Previous Demo: </strong>Real-Time Geolocation Service with Node.js
+                </a>
+                <span class="right">
+                    <a href="http://tympanus.net/codrops/?p=11372">
+                        <strong>Back to the Codrops Article</strong>
+                    </a>
+                </span>
+				-->
+            </div><!--/ Codrops top bar -->
+			
+			<header>
+			
+				<h1>Need <strong>study help?</strong></h1>
+				<h2>Enter your student ID and password below to start chatting with Blair's student tutors!</h2>
+				
+				<nav class="codrops-demos">
+					<!--
+					<a class="current-demo" href="index.html">Demo 1</a>
+					<a href="index2.html">Demo 2</a>
+					<a href="index3.html">Demo 3</a>
+					<a href="index4.html">Demo 4</a>
+					<a href="index5.html">Demo 5</a>
+					-->
+				</nav>
+
+				<div class="support-note">
+					<span class="note-ie">Sorry, only modern browsers.</span>
+				</div>
+				
+			</header>
+			
+			<div class="main-form">
+				<form class="form-1">
+					<p class="field">
+						<input type="text" name="login" placeholder="Student ID #">
+						<i class="icon-user icon-large"></i>
+					</p>
+						<p class="field">
+							<input type="password" name="password" placeholder="Password">
+							<i class="icon-lock icon-large"></i>
+					</p>
+					<p class="submit">
+						<button type="submit" name="submit"><i class="icon-arrow-right icon-large"></i></button>
+					</p>
+				</form>
+			</div> <!--close main div-->
+        </div>
+    </body>
+</html>
